8ba3a3ca13809081f112c92629835a0aa56c8fc7
Miguel Dagraza <migueldagraza@gmail.com>
2026-06-12 14:15:55 +0200

    refactor: refactor report service calls and update dependencies for report operations

--- app/routers/reports.py
+++ app/routers/reports.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends, status, Query, Response, UploadFile, File
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_db
-from app.dependencies.auth import Get_Current_User
 from app.schemas.report import *
 from app.models.report import Report
-from app.services.reports import create_new_report, get_all_reports, get_report_by_id, delete_report_by_id, update_report_by_id, save_files, get_report_images
 from app.services import reports as report_services
-from app.dependencies.reports import valid_report_id
+from app.dependencies import reports as report_dependencies
+from app.dependencies import auth as auth_dependencies
 
 
 router = APIRouter(prefix="/reports", tags=["reports"])
@@ -21,7 +20,7 @@ async def get_reports(response: Response, page: int = Query(1, ge=1, description
     page_size: int = Query(10, ge=1, description="Cantidad de registros por pagina"),
     db: AsyncSession = Depends(get_db)):
 
-    reports, total_items = await get_all_reports(page, page_size, db)
+    reports, total_items = await report_services.get_all_reports(page, page_size, db)
 
     await _set_pagination_header(response, page_size, total_items) #Añadir a la cabecera el total paginas e items
 
@@ -33,40 +32,40 @@ async def get_reports(response: Response, page: int = Query(1, ge=1, description
 
 @router.get("/{id}", response_model=ReportResponse)
 async def get_report(id: int, db: AsyncSession = Depends(get_db)):
-    return await get_report_by_id(id, db)
+    return await report_services.get_report_by_id(id, db)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=ReportResponse)
 async def create_report(report_data: ReportCreate, db: AsyncSession = Depends(get_db)):
-    return await create_new_report(report_data, db)
+    return await report_services.create_new_report(report_data, db)
 
 
 @router.patch("/{id}", response_model=ReportResponse)
 async def update_report(id: int, report_data: ReportUpdate, db: AsyncSession = Depends(get_db)):
-    return await update_report_by_id(id, report_data, db)
+    return await report_services.update_report_by_id(id, report_data, db)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_report(id: int, db: AsyncSession = Depends(get_db)):
-    await delete_report_by_id(id, db)
+    await report_services.delete_report_by_id(id, db)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 #Imágenes
 @router.post("/{id}/images", status_code=status.HTTP_201_CREATED) #TODO: Añadir limites de cantidad y validaciones de tipo de archivo
-async def upload_report_image(file: UploadFile = File(...), report: Report = Depends(valid_report_id), db: AsyncSession = Depends(get_db), current_user=Depends(Get_Current_User())):
-    saved_files = await save_files([file], report.id, current_user[0].id, db)
+async def upload_report_image(file: UploadFile = File(...), report: Report = Depends(report_dependencies.valid_report_id), db: AsyncSession = Depends(get_db), current_user=Depends(auth_dependencies.Get_Current_User())):
+    saved_files = await report_services.save_files([file], report.id, current_user[0].id, db)
     return {"message": f"Imagen subida para el reporte {report.title} | {report.id}",
             "saved_files": saved_files}
 
 @router.get("/{id}/images", response_model=ReportImagesResponse)
-async def get_report_images(report: Report = Depends(valid_report_id), db: AsyncSession = Depends(get_db)):
+async def get_report_images(report: Report = Depends(report_dependencies.valid_report_id), db: AsyncSession = Depends(get_db)):
     images = await report_services.get_report_images(report.id, db)
     return ReportImagesResponse(num_images=len(images), images=images)
 
 @router.delete("/{id}/image/{image_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_report_image(image_id: int, report: Report = Depends(valid_report_id), db: AsyncSession = Depends(get_db)):
+async def delete_report_image(image_id: int, report: Report = Depends(report_dependencies.valid_report_id), db: AsyncSession = Depends(get_db)):
     await report_services.delete_report_image(report.id, image_id, db)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.delete("/{id}/images", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_all_report_images(report: Report = Depends(valid_report_id), db: AsyncSession = Depends(get_db)):
+async def delete_all_report_images(report: Report = Depends(report_dependencies.valid_report_id), db: AsyncSession = Depends(get_db)):
     await report_services.delete_all_report_images(report.id, db)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
